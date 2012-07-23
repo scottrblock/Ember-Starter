f3d955b4f0f06e5ec7adbc177693cbbd1148ce60
Scott Block <scott.block@hungrymachine.com>
2012-07-23 12:51:22 -0400

    I have no idea what i'm doing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
-var App = Em.Application.create();
+window.App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+
+App.Quote = Em.Object.extend({
+  text: null,
+  possibleAuthours: [],
+  realAuthor: null
 });
+
+App.Author = Em.Object.extend({
+  firstName: '',
+  lastName: '',
+  fullName: function() {
+     return this.get('firstName') + ' ' + this.get('lastName');
+   }.property()
+});
+
+App.User = Em.Object.extend({
+  firstName: '',
+  lastName: '',
+  score: 0,
+});
+
+App.CurrentUser = App.User.create();
+
+App.authors = [
+  App.wale = App.Author.create({
+    firstName: "Wale",
+  }),
+
+  App.doug = App.Author.create({
+    firstName: "Doug",
+    lastName: "March"
+  })
+]
+
+
+App.AuthorController = Em.ArrayController.create({
+  content: App.authors
+});
+
